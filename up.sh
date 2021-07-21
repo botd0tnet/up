64190e5cb721bf3828d17edf29682bf7c1dc43bc
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2021-07-21 16:41:13 +0000

    Update up.sh

--- up.sh
+++ up.sh
@@ -7,7 +7,7 @@ do
 	nym=${kt}${i}
 	cd /home/${nym}
 	sudo rm -r /home/${nym}/nym-mixnode_linux_x86_64
-	sudo wget https://github.com/nymtech/nym/releases/download/v0.10.1/nym-mixnode_linux_x86_64
+	sudo wget https://github.com/nymtech/nym/releases/download/v0.11.0/nym-mixnode_linux_x86_64
 	sudo chmod +x nym-mixnode_linux_x86_64
 	sudo -u ${nym} -H ./nym-mixnode_linux_x86_64 upgrade  --id 'NymMixNode'
 	printf "\n\n"
